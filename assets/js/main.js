c6f24a3cac3764254a833e27642b7f6e818149f8
John Silkey <John Silkey>
2017-05-16 15:00:21 -0700

    split up my js files for readability

---
+++ assets/js/main.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+
+	var topics = ['Bernie Sanders', 'Ostriches'];
+
+	// loops through array and prints each button
+	$.each(topics, function(i){
+		printButton(topics[i]);
+	});
+
+	// event listener for clicking on buttons
+	$(document).on('click', '.giphy-button', function(){
+		// calls function and sends it giphy data
+		displayGiphies($(this).data().giphies);
+	});
+
+	// event listener for clicking on an image and changing its state
+	$(document).on('click', '.giphy-img', function(){
+		// calls function and sends it this clicked image
+		changeImgState($(this));
+	});
+
+	// event listener clicking the submit button
+	$('#submit-btn').on('click', function(e){
+		//prevents page auto-refresh
+		e.preventDefault();
+
+		// declares string var equal to the input minus any trailing white space on either side
+		var userInpBtnName = $('#topic-input').val().trim();
+
+		// empties input text field
+		$('#topic-input').val('');
+
+		// pushes the new button onto the topics array. This isn't necessary for this program
+		// at this point but it might be helpful later on to have a running list of topics.
+
+		// calls printButton to put button on DOM and also to store giphy api data onto it 
+		printButton(userInpBtnName);
+	});
+
+}); // end of document ready
---
+++ assets/js/methods.js
@@ -0,0 +1,82 @@
+// GLOBAL FUNCTIONS
+
+// prints a received button according to a string with its name.
+// also calls storeDataInButton() at the end.
+function printButton(btnName) {
+	// trims the white space before and after the button name
+	btnName = btnName.trim();
+
+	// prepends button on the DOM
+	var newBtn = $('<button>');
+	newBtn.addClass('giphy-button btn btn-primary')
+		.text(btnName)
+		.attr('value', btnName.replace(/\s+/g, '+')) // value equals btnName with spaces replaced by +'s
+		.prependTo('#buttons-list');
+
+	// sends the button to storeDataInButton() to store giphy data onto it
+	storeDataInButton(newBtn);
+}
+
+// this function makes an ajax request and stores the response data onto the DOM button itself
+function storeDataInButton(thisBtn) {
+	// sets queryTerm as the value of the received button argument, i.e. the button name
+	var queryTerm = thisBtn.val();	
+	var giphyApiKey = 'dc6zaTOxFJmzC';
+
+	// query URL for the giphy API
+	var queryURL = 'http://api.giphy.com/v1/gifs/search?q=' + queryTerm 
+					+ '&api_key=' + giphyApiKey + '&limit=2';
+	// AJAX request
+	$.ajax({
+		method: "GET",
+		url: queryURL
+	}).done(function(r){
+		// the promise function simply stores the relevant response data directly onto 
+		// the DOM element itself.
+		thisBtn.data('giphies', r.data);
+	});
+}
+
+// function for displaying giphies stored in an array of giphy objects
+function displayGiphies (giphiesAry) {
+	$('#giphy-results').empty();
+	$('<p>').text('Click on an image and see what happens!').appendTo('#giphy-results');
+	console.log(giphiesAry);
+
+	// loops through array of giphies
+	$.each(giphiesAry, function(i){
+		// for the sake of encapsulation
+		var thisImg = giphiesAry[i].images;
+
+		var giphyWrapper = $('<div>');
+		giphyWrapper.addClass('giphy-wrapper');
+
+		var ratingsText = $('<p>');
+		ratingsText.text('Rating: ' + giphiesAry[i].rating)
+				.appendTo(giphyWrapper);
+
+		var giphyImg = $('<img>');
+		giphyImg.attr('alt', giphiesAry[i].bitly_gif_url)
+				.attr('src', thisImg.fixed_height_still.url)
+				.addClass('giphy-img')
+				.data('motion_state', thisImg.fixed_height.url)
+				.data('still_state', thisImg.fixed_height_still.url)
+				.data('current_state', 'still')
+				.appendTo(giphyWrapper);
+
+		$('#giphy-results').append(giphyWrapper);
+	});	
+}
+
+// toggles the image state between still and in motion
+function changeImgState(img) {
+	// sets conditions for changing states
+	if (img.data('current_state') === 'still') {
+		img.attr('src', img.data('motion_state'))
+			.data('current_state', 'motion');
+	}
+	else if (img.data('current_state') === 'motion') {
+		img.attr('src', img.data('still_state'))
+			.data('current_state', 'still');
+	}	
+}
